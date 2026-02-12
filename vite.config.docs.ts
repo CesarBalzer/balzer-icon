1152ccf4f5f2781ee4d08132c21353b32d88ae80
Cesar Balzer <cesar.edenir@mkbank.com.br>
2026-02-12 17:37:11 -0300

    fix: adjusts gh pages

--- vite.config.docs.ts
+++ vite.config.docs.ts
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'balzer-icon',
-  build: {
-    outDir: 'docs'
-  },
-  plugins: [
-    react(),
-    svgr()
-  ]
-})
+	base: '/balzer-icon/',
+	build: {
+		outDir: 'docs'
+	},
+	plugins: [react(), svgr()]
+});
